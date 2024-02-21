e0f959c2ee5076acb6911c34b820d437c8e62d96
mmm25002500 <marryhan25648@gmail.com>
2024-02-22 04:10:51 +0800

    fix: sidebar not found error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import Navbar from "@/components/Layout/Navbar";
-import Sidebar from "@/components/Sidebar";
 
 const Home = () => {
   return (
     <>
-      {/* <Sidebar></Sidebar> */}
     </>
   );
 }
