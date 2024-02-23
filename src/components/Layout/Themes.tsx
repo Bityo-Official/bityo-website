345c47309ce06b10ba67cc9d52eec5dbdde2ec85
mmm25002500 <marryhan25648@gmail.com>
2024-02-24 05:00:14 +0800

    fix: theme switch bug (default light theme)

--- src/components/Layout/Themes.tsx
+++ src/components/Layout/Themes.tsx
@@ -1,34 +1,41 @@
 import { useEffect, useState } from "react";
-import { useCookies } from 'react-cookie';
 import { useTheme } from "next-themes";
-import Image from 'next/image';
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 const Themes = () => {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => setMounted(true), [])
+  // 掛載 mounted
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // 切換主題
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  // 確保 component 掛載後再 render
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className="w-[6.125rem] h-[2.5rem] rounded-full p-1 bg-gray-200 dark:bg-[#6B6B6B] relative transition-colors duration-500 ease-in">
+    <button onClick={toggleTheme} className="w-[6.125rem] h-[2.5rem] rounded-full p-1 bg-gray-200 dark:bg-[#6B6B6B] relative transition-colors duration-500 ease-in">
       {
-        // 如果是 dark mode
-        theme === 'dark' ? (
-          <div
-            className="rounded-full w-8 h-8 border-gray-700 dark:bg-gray-700 relative dark:ml-[3.6rem] pointer-events-none transition-all duration-300 ease-out">
+        theme === 'dark' || (!theme && resolvedTheme === 'dark') ? (
+          <div className="rounded-full w-8 h-8 border-gray-700 dark:bg-gray-700 relative dark:ml-[3.6rem] pointer-events-none transition-all duration-300 ease-out">
             <MoonIcon className="w-[50%] h-[50%] mr-auto ml-auto inline-block align-middle" />
           </div>
         ) : (
-          // 如果是 light mode
-          <div
-            className="rounded-full border-gray-300 border-[1px] w-8 h-8 bg-white relative dark:ml-0 pointer-events-none transition-all duration-300 ease-out">
+          <div className="rounded-full border-gray-300 border-[1px] w-8 h-8 bg-white relative dark:ml-0 pointer-events-none transition-all duration-300 ease-out">
             <SunIcon className="w-[50%] h-[50%] mr-auto ml-auto inline-block align-middle" />
           </div>
         )
       }
     </button>
-  )
-}
+  );
+};
 
 export default Themes;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <Layout>
         <NextNProgress height={2} color="#17FFAC" options={{ easing: 'ease', speed: 500 }} />
         <Component {...pageProps} />
