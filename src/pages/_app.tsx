671192f64e069bd7b98c410dc15e3c1039b2208f
mmm25002500 <marryhan25648@gmail.com>
2024-02-24 04:54:06 +0800

    fix: theme switch bug (default light theme)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ config.autoAddCss = false;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Layout>
         <NextNProgress height={2} color="#17FFAC" options={{ easing: 'ease', speed: 500 }} />
         <Component {...pageProps} />
